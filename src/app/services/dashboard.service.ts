2d6f8695996847b1607584ce7a3049f2b076c391
Ludger Jean Louis <ludgerjeanlouis@gmail.com>
2023-11-08 20:13:25 -0300

    nova alteração

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -8,7 +8,8 @@ import { Observable } from 'rxjs';
 export class DashboardService {
 
   //public API = 'http://localhost:8080';
-  public API = 'https://api-portfoli-2478bc1714ca.herokuapp.com/';
+  //public API = 'https://api-portfoli-2478bc1714ca.herokuapp.com/';
+  public API = 'https://deploy-render-djf6.onrender.com/';
   public CONTROLLER = this.API + '/portfolio';
 
   constructor(private http: HttpClient) { }
